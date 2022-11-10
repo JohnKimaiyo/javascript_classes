a30cc30ba8ed9ab970190403ddae370e8a205c89
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-10 09:38:43 +0300

    james login logic

---
+++ project 1/james.js
@@ -0,0 +1,15 @@
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+  }
+  login() {
+    console.log(this.email, "just login in");
+  }
+  logout() {
+    console.log(this.email, "just logged out");
+  }
+}
+
+var userone = new User("james@gmail.com", "james");
+userone.login();
