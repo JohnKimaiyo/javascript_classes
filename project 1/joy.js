a0775f62d77ebe66fb00813fc4d1b460dbea4fb3
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-10 10:02:36 +0300

    joy user logic

---
+++ project 1/joy.js
@@ -0,0 +1,16 @@
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+  }
+  login() {
+    console.log(this.email, "just logged in");
+  }
+  logout() {
+    console.log(this.email, "just logged out");
+  }
+}
+
+var userone = new User("joy@gmail.com", "joy");
+
+userone.login();
