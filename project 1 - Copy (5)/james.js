09a1bc7e147cb05af1962d8975dd734c193faf08
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-11 10:33:08 +0300

    james login and logout logic

---
+++ project 1 - Copy (5)/james.js
@@ -0,0 +1,16 @@
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+  }
+  login() {
+    console.log(this.email, "just logged in");
+  }
+  logout() {
+    console.log(this.email, "just logged out");
+  }
+}
+
+var userone = new User("james@gmail.com", "james");
+userone.login();
+userone.logout();
