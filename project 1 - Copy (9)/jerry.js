ed59f760d28f60674786f202a4887eb9d39a42fa
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-12 17:58:50 +0300

    user jerry has logged in

---
+++ project 1 - Copy (9)/jerry.js
@@ -0,0 +1,16 @@
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+  }
+  login() {
+    console.log(this.name, this.email, "just logged in");
+  }
+  logout() {
+    console.log(this.name, this.email, "just logged out");
+  }
+}
+
+const userone = new User("jerry", "jerry@gmail.com");
+userone.login();
+userone.logout();
