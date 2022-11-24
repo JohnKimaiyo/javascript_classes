df94258f9c737ceaa1eefba5bf7ac1c11f8d8172
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-24 06:27:00 -0800

    user one login logout logic

---
+++ project 17/john.js
@@ -0,0 +1,15 @@
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name
+    }
+    login() {
+        console.log(this.email, "just logged in")
+    }
+    logout() {
+        console.log(this.email, "just logged oit")
+    }
+}
+const userone = new User("john@gmail.com", "john");
+userone.login();
+userone.logout();
